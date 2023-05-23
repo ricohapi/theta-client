b6875998188be59bac622dab2633fe0d58b89b4b
imago <imago@src.ricoh.co.jp>
2023-05-23 15:57:30 +0900

    Add 7200 to option MaxRecordableTime

--- flutter/lib/theta_client_flutter.dart
+++ flutter/lib/theta_client_flutter.dart
@@ -1765,7 +1765,16 @@ enum MaxRecordableTimeEnum {
   time_300('RECORDABLE_TIME_300'),
 
   /// Maximum recordable time. 1500sec for other than SC2.
-  time_1500('RECORDABLE_TIME_1500');
+  time_1500('RECORDABLE_TIME_1500'),
+
+  /// Maximum recordable time. 7200sec for Theta X version 2.00.0 or later,
+  /// only for 5.7K 2/5/10fps and 8K 2/5/10fps.
+  /// If you set 7200 seconds in 8K 10fps mode and then set back to 4K 30fps mode,
+  /// the max recordable time will be overwritten to 1500 seconds automatically.
+  time_7200('RECORDABLE_TIME_7200'),
+
+  /// Just used by getMySetting/setMySetting command
+  doNotUpdateMySettingCondition('DO_NOT_UPDATE_MY_SETTING_CONDITION');
 
   final String rawValue;
   const MaxRecordableTimeEnum(this.rawValue);
