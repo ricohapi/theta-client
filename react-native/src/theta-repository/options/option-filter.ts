78af0ac1d57ffcd8f3f9c0085f6c750460bfd48d
osakila <osaki@lassic.co.jp>
2023-06-21 15:44:24 +0900

    Add option filter item

---
+++ react-native/src/theta-repository/options/option-filter.ts
@@ -0,0 +1,25 @@
+/** Image processing filter. */
+export const FilterEnum = {
+  /** No filter. */
+  OFF: 'OFF',
+  /**
+   * DR compensation.
+   *
+   * RICOH THETA X is not supported.
+   */
+  DR_COMP: 'DR_COMP',
+  /** Noise reduction. */
+  NOISE_REDUCTION: 'NOISE_REDUCTION',
+  /** HDR. */
+  HDR: 'HDR',
+  /**
+   * Handheld HDR.
+   *
+   * RICOH THETA Z1 firmware v1.20.1 or later and RICOH THETA V firmware v3.10.1 or later.
+   * RICOH THETA X is not supported.
+   */
+  HH_HDR: 'HH_HDR',
+} as const;
+
+/** type definition of FilterEnum */
+export type FilterEnum = (typeof FilterEnum)[keyof typeof FilterEnum];
