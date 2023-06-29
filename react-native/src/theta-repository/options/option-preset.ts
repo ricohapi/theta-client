9ecbecb4e55e80953e656ce1cc86c990e06c8f3e
imago <imago@src.ricoh.co.jp>
2023-06-29 11:46:44 +0900

    Run lint to newly created files

--- react-native/src/theta-repository/options/option-preset.ts
+++ react-native/src/theta-repository/options/option-preset.ts
@@ -5,17 +5,15 @@
  **/
 
 export const PresetEnum = {
-    /** Preset "Face" mode suitable for portrait shooting just for Theta SC2. */
-    FACE: 'FACE',
-    /** Preset "Night View" mode just for Theta SC2. */
-    NIGHT_VIEW: 'NIGHT_VIEW',
-    /** Preset "Lens-by-Lens Exposure" mode just for Theta SC2. */
-    LENS_BY_LENS_EXPOSURE: 'LENS_BY_LENS_EXPOSURE',
-    /** Preset "Room" mode just for SC2 for business. */
-    ROOM: 'ROOM',
-  } as const;
-  
-  /** Type definition of PresetEnum */
-  export type PresetEnum =
-   (typeof PresetEnum)[keyof typeof PresetEnum];
-  
+  /** Preset "Face" mode suitable for portrait shooting just for Theta SC2. */
+  FACE: 'FACE',
+  /** Preset "Night View" mode just for Theta SC2. */
+  NIGHT_VIEW: 'NIGHT_VIEW',
+  /** Preset "Lens-by-Lens Exposure" mode just for Theta SC2. */
+  LENS_BY_LENS_EXPOSURE: 'LENS_BY_LENS_EXPOSURE',
+  /** Preset "Room" mode just for SC2 for business. */
+  ROOM: 'ROOM',
+} as const;
+
+/** Type definition of PresetEnum */
+export type PresetEnum = (typeof PresetEnum)[keyof typeof PresetEnum];
