d135a81595e8c9b0329793e4c52796e8ceb926af
imago <imago@src.ricoh.co.jp>
2023-07-12 11:58:33 +0900

    Rewrite test codes

--- kotlin-multiplatform/src/androidTest/kotlin/com/ricoh360/thetaclient/UpdateFirmwareOnTheta.kt
+++ kotlin-multiplatform/src/androidTest/kotlin/com/ricoh360/thetaclient/UpdateFirmwareOnTheta.kt
@@ -1,5 +1,6 @@
 package com.ricoh360.thetaclient
 
+import android.os.strictmode.DiskReadViolation
 import com.goncalossilva.resources.Resource
 import com.ricoh360.thetaclient.repository.UpdateFirmwareTest
 import io.ktor.http.HttpStatusCode
@@ -25,25 +26,43 @@ class UpdateFirmwareOnTheta {
     }
 
     @Test
-    fun updateFirmwareSc2_bTest() = runTest(dispatchTimeoutMs = 180_000L) {
-        val dir = "/media/sf_vmshare/ex" // directory firmware files exist
-        val file = "bx1_v641.frm" // one of bx1_v652.frm, bx1_v641.frm
-        lateinit var firmware: ByteArray
-        kotlin.runCatching {
-            firmware = readFile(dir, file)
-        }.onFailure {
-            println(it.toString())
-            assertTrue(false, "updateFirmwareTest")
-        }
-        println("Firmware size: ${firmware.size}")
+    fun updateFirmwareSc2_bTest() = runTest(dispatchTimeoutMs = TIMEOUT) {
 
-        lateinit var apiPath: String
-        kotlin.runCatching {
-            apiPath = System.getenv(UpdateFirmwareTest.FIRMWARE_UPDATE_API_ENV_NAME) ?: ""
+
+        val thetaRepository = kotlin.runCatching {
+            ThetaRepository.newInstance(
+                endpoint,
+                timeout = ThetaRepository.Timeout(requestTimeout = TIMEOUT, socketTimeout = TIMEOUT)
+            )
+        }.onFailure {
+            println(it)
+            return@runTest
+        }.getOrNull()
+
+        if (thetaRepository?.cameraModel != ThetaRepository.ThetaModel.THETA_SC2_B) {
+            println("Connected Theta is not SC2 for business but ${thetaRepository?.cameraModel}" )
+            return@runTest
+        }
+
+        val firmFile = when (thetaRepository.getThetaInfo().firmwareVersion) {
+            VERSION_LATEST -> FILE_PREVIOUS
+            else -> FILE_LATEST
+        }
+
+        val firmware: ByteArray? = kotlin.runCatching {
+            readFile(DIR, firmFile)
+        }.onFailure {
+            println(it)
+            return@runTest
+        }.getOrNull()
+        println("Firmware size: ${firmware?.size}")
+
+        val apiPath = kotlin.runCatching {
+            System.getenv(UpdateFirmwareTest.FIRMWARE_UPDATE_API_ENV_NAME)
         }.onFailure {
             println("${UpdateFirmwareTest.FIRMWARE_UPDATE_API_ENV_NAME} can not be accessed so updateFirmwareTest() is skipped")
-            assertTrue(false, "Can't get API path")
-        }
+            return@runTest
+        }.getOrNull()
         println("API path: $apiPath")
 
         MockApiClient.onRequest = { request ->
@@ -51,9 +70,13 @@ class UpdateFirmwareOnTheta {
             assertEquals(request.url.encodedPath, apiPath, "request path")
             ByteReadChannel(Resource("src/commonTest/resources/updateFirmware/update_firmware.done.json").readText())
         }
-        val thetaRepository = ThetaRepository(endpoint)
-        thetaRepository.cameraModel = ThetaRepository.ThetaModel.THETA_SC2_B
-        thetaRepository.updateFirmware(apiPath, listOf(firmware), listOf(file))
+
+        kotlin.runCatching {
+            thetaRepository.updateFirmware(apiPath!!, listOf(firmware!!), listOf(firmFile))
+        }.onFailure {
+            println(it)
+            return@runTest
+        }
         assertTrue(true, "call updateFirmware()")
     }
 
@@ -69,5 +92,15 @@ class UpdateFirmwareOnTheta {
     companion object {
         // Environment variable name that holds the path of Theta firmware update API.
         const val FIRMWARE_UPDATE_API_ENV_NAME= "THETA_FU_API_PATH"
+        // Timeout for socket and coroutine
+        const val TIMEOUT = 300_000L
+        // lastest firmware version of SC2 for business
+        const val VERSION_LATEST = "06.52"
+        // latest firmware file of SC2 for business
+        const val FILE_LATEST = "bx1_v652.frm"
+        // previous firmware file of SC2 for business
+        const val FILE_PREVIOUS = "bx1_v641.frm"
+        // directory firmware files exist
+        val DIR = "/media/sf_vmshare/ex"
     }
 }
