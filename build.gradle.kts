b142f7712a31bddf84554b0e3b03d685c0bf0fb5
松岡　侑凛 <129148471+LassicYM@users.noreply.github.com>
2023-11-17 09:26:58 +0900

    update kotlin & RN for XCode15.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,16 @@
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-        classpath("com.android.tools.build:gradle:7.2.2")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.8.20")
-        classpath("org.jetbrains.dokka:versioning-plugin:1.8.20")
-    }
+plugins {
+    //trick: for the same plugin versions in all sub-modules
+    id("com.android.library").version("8.1.2").apply(false)
+    kotlin("multiplatform").version("1.9.10").apply(false)
+    kotlin("plugin.serialization").version("1.9.10").apply(false)
+    id("org.jetbrains.dokka").version("1.9.10")
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.dokka:versioning-plugin:1.9.10")
     }
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
 }
