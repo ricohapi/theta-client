d154d7e13d233463ac1d1bc3816aad3d43fb8521
osakila <osaki@lassic.co.jp>
2024-01-11 14:48:14 +0900

    Update version to 1.7.1

--- kotlin-multiplatform/build.gradle.kts
+++ kotlin-multiplatform/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     dokkaPlugin("org.jetbrains.dokka:versioning-plugin:1.9.10")
 }
 
-val thetaClientVersion = "1.7.0"
+val thetaClientVersion = "1.7.1"
 group = "com.ricoh360.thetaclient"
 version = thetaClientVersion
 
