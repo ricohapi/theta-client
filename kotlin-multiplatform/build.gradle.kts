832cae6dc3181fc325851c1728c4874cd5be4b0f
松岡　侑凛 <129148471+LassicYM@users.noreply.github.com>
2024-10-30 14:51:37 +0900

    Update version to 1.11.0

--- kotlin-multiplatform/build.gradle.kts
+++ kotlin-multiplatform/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     dokkaPlugin("org.jetbrains.dokka:versioning-plugin:1.9.10")
 }
 
-val thetaClientVersion = "1.10.2"
+val thetaClientVersion = "1.11.0"
 group = "com.ricoh360.thetaclient"
 version = thetaClientVersion
 
